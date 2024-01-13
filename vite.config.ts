c208d2a69542b8b5c354cb9856da6cdba2144aa2
ws <ws3035347615@163.com>
2024-01-13 10:20:17 +0800

    99588WS change port

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,43 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import path from "path";
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+// import AutoImport from "unplugin-auto-import/vite";
+// import Components from "unplugin-vue-components/vite";
+// import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+// import path from "path";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     AutoImport({
+//       resolvers: [ElementPlusResolver()],
+//     }),
+//     Components({
+//       resolvers: [ElementPlusResolver()],
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve("./src"), // @代替src
+//     },
+//   },
+// });
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+function pathResolve(dir: string): string {
+  return resolve(process.cwd(), '.', dir)
+};
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve("./src"), // @代替src
-    },
+      '@': pathResolve('src')
+    }
   },
-});
+  server: {
+    host: '0.0.0.0', //ip地址
+    port: 8080, //端口号
+    open: true //启动后是否自动打开浏览器
+  }
+})
